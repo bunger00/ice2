e6ba2e391fd873d74fe197b10b50e82cea9112fd
bunger00 <bjornu@gmail.com>
2025-02-17 00:53:16 +0100

    Updated GitHub Pages configuration

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ICE-meeting/',  // Dette er riktig siden det matcher repo-navnet
 }) 
